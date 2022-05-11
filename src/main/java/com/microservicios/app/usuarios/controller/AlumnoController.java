b54a3da7831f6ea406df5d4d4952045f4a204015
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-05-11 13:29:07 -0300

    implementando clase generica

--- src/main/java/com/microservicios/app/usuarios/controller/AlumnoController.java
+++ src/main/java/com/microservicios/app/usuarios/controller/AlumnoController.java
@@ -2,42 +2,25 @@ package com.microservicios.app.usuarios.controller;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.formacionbdi.microservicios.commons.controller.CommonController;
 import com.microservicios.app.usuarios.models.entity.Alumno;
 import com.microservicios.app.usuarios.services.AlumnoService;
 
 @RestController
-public class AlumnoController {
-	@Autowired
-	private AlumnoService service;
+public class AlumnoController extends CommonController<Alumno, AlumnoService>{
 
-	@GetMapping
-	public ResponseEntity<?> listar() {
-			return ResponseEntity.ok().body(service.findAll());
-	}
-	@GetMapping("/{id}")
-	public ResponseEntity<?> ver(@PathVariable Long id){
-		Optional<Alumno> o=service.findById(id);
-		if(o.isEmpty()) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok(o.get());
-	}
-	@PostMapping
-	public ResponseEntity<?> crear(@RequestBody Alumno alumno){
-		Alumno  alumnoDb=service.save(alumno);
-		return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
-	}
+
+	
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Alumno alumno,@PathVariable Long id){
 		Optional<Alumno> o=service.findById(id);
@@ -52,11 +35,7 @@ public class AlumnoController {
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb)); 
 	}
-	@DeleteMapping("/{id}")
-	public ResponseEntity<?> eliminar(@PathVariable Long id){
-		service.deleteById(id);
-	return ResponseEntity.noContent().build();
-}
+	
 }
 
 
--- src/main/java/com/microservicios/app/usuarios/services/AlumnoService.java
+++ src/main/java/com/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,12 +1,11 @@
 package com.microservicios.app.usuarios.services;
 
-import java.util.Optional;
 
+
+import com.formacionbdi.microservicios.commons.service.CommonService;
 import com.microservicios.app.usuarios.models.entity.Alumno;
 
-public interface AlumnoService {
-public Iterable<Alumno> findAll();
-public Optional<Alumno> findById(Long id);
-public Alumno save(Alumno alumno);
-public void deleteById(Long id);
+public interface AlumnoService extends CommonService<Alumno>{
+
+
 }
