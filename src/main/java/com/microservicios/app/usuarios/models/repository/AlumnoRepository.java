6423f22c8c1f6bca1e01bac9245ae31dba8ff953
Gionnelly <80008338+gvielza@users.noreply.github.com>
2022-05-27 12:22:43 -0300

    paginacion lista

--- src/main/java/com/microservicios/app/usuarios/models/repository/AlumnoRepository.java
+++ src/main/java/com/microservicios/app/usuarios/models/repository/AlumnoRepository.java
@@ -3,12 +3,12 @@ package com.microservicios.app.usuarios.models.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.commons.alumnos.models.entity.Alumno;
-
-public interface AlumnoRepository extends CrudRepository<Alumno, Long> {
+//agregar paginación,agregamos al repository en ves del Crud por PagingAndSortingRepository
+public interface AlumnoRepository extends PagingAndSortingRepository<Alumno, Long> {
 
 	@Query("select a from Alumno a where a.nombre like %?1% or a.apellido like %?1%")
 	public List<Alumno> findByNombreoApellido(String term);
